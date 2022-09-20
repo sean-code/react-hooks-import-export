7528a8763b813d87b7fb558ebe07e7d6095b578f
sean-code <john.nganga@student.moringaschool.com>
2022-09-20 18:38:07 +0300

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ColoradoStateParks from "./ColoradoStateParks";
+import ColoradoStateParks from "./ColoradoStateParks.js";
+
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
